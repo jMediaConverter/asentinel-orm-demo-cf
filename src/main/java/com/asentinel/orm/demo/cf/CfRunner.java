36ef33a41e94cb046a8f98717017a6ef68323b05
horatiu.dan <horatiu.dan@tangoe.com>
2024-11-26 16:59:34 +0200

    Creates a CarService to better outline the ORM features.

--- src/main/java/com/asentinel/orm/demo/cf/CfRunner.java
+++ src/main/java/com/asentinel/orm/demo/cf/CfRunner.java
@@ -1,17 +1,15 @@
 package com.asentinel.orm.demo.cf;
 
-import com.asentinel.common.jdbc.DefaultObjectFactory;
-import com.asentinel.common.orm.AutoEagerLoader;
-import com.asentinel.common.orm.OrmOperations;
 import com.asentinel.common.orm.mappers.dynamic.DefaultDynamicColumn;
 import com.asentinel.common.orm.mappers.dynamic.DynamicColumn;
-import com.asentinel.common.orm.mappers.dynamic.DynamicColumnsEntityNodeCallback;
-import com.asentinel.common.orm.persist.UpdateSettings;
 import com.asentinel.orm.demo.cf.domain.CarManufacturer;
 import com.asentinel.orm.demo.cf.domain.CarModel;
 import com.asentinel.orm.demo.cf.domain.CarType;
 import com.asentinel.orm.demo.cf.domain.CustomFieldsCarManufacturer;
-import jakarta.annotation.PostConstruct;
+import com.asentinel.orm.demo.cf.service.CarService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
@@ -23,72 +21,61 @@ import java.util.List;
 import java.util.Map;
 
 @Component
-public class CfRunner {
+public class CfRunner implements CommandLineRunner {
 
-	private final OrmOperations orm;
+	private static final Logger LOG = LoggerFactory.getLogger(CfRunner.class);
 
-    public CfRunner(OrmOperations orm) {
-        this.orm = orm;
+	private final CarService carService;
+
+    public CfRunner(CarService carService) {
+        this.carService = carService;
     }
 
-    @PostConstruct
-	private void run() throws IOException {
+    @Override
+	public void run(String... args) throws IOException {
 		List<DynamicColumn> dynamicColumns = new ArrayList<>();
+
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
 			for (;;) {
-				System.out.println("Input the column name (hit enter to stop):");
+				System.out.println("---> Input the column name (hit enter to stop):");
 				String name = reader.readLine();
 				if (name.isBlank()) {
 					break;
 				}
 
-				System.out.println("Input the column type, only varchar and int are supported (hit enter to stop):");
+				System.out.println("---> Input the column type, only 'varchar' and 'int' are supported (hit enter to stop):");
 				String type = reader.readLine();
 				if (type.isBlank()) {
 					break;
 				}
 
-				orm.getSqlQuery()
-						.getJdbcOperations()
-						.update("alter table CarManufacturers add column " +  name + " " + type );
+				carService.addManufacturerField(name, type);
 
 				dynamicColumns.add(new DefaultDynamicColumn(name, columnType(type)));
 			}
 
-			System.out.println(dynamicColumns);
-			
-			
-			// save the 2 manufacturers in the DB
-			CarManufacturer mazda0 = saveManufacturer(reader, "Mazda", dynamicColumns);
+			LOG.info("Car Manufacturer dynamic columns: \n{}", dynamicColumns);
+
+			// create the 2 manufacturers and 2 models
+			CarManufacturer mazda0 = addManufacturer(reader, "Mazda", dynamicColumns);
 			CarModel mx5 = new CarModel("MX5", CarType.CAR, mazda0);
 			CarModel cx60 = new CarModel("CX60", CarType.SUV, mazda0);
-			orm.update(mx5, cx60);
+			carService.createModels(mx5, cx60);
 			
-			saveManufacturer(reader, "Honda", dynamicColumns);
+			addManufacturer(reader, "Honda", dynamicColumns);
 		}
 
-		// load the mazda manufacturer from the db
-		CustomFieldsCarManufacturer mazda1 = orm.newSqlBuilder(CustomFieldsCarManufacturer.class)
-				.select(
-						AutoEagerLoader.forPath(CarManufacturer.class, CarModel.class),
-						new DynamicColumnsEntityNodeCallback<>(
-								new DefaultObjectFactory<>(CustomFieldsCarManufacturer.class),
-								dynamicColumns
-					))
-				.where().column("name").eq("Mazda")
-				.execForEntity();
-		System.out.println("Loaded manufacturer: " + mazda1);
-
-		System.out.println("Manufacturer car models: ");
-		mazda1.getModels()
-				.forEach(System.out::println);
+		// read mazda manufacturer
+		CarManufacturer mazda1 = carService.findManufacturerByName("Mazda", dynamicColumns);
+		LOG.info("Loaded manufacturer: {}", mazda1);
+		LOG.info("Manufacturer car models: {}", mazda1.getModels());
 	}
 	
-	private CarManufacturer saveManufacturer(BufferedReader reader,
-											 String name, List<DynamicColumn> dynamicColumns) throws IOException {
+	private CarManufacturer addManufacturer(BufferedReader reader,
+											String name, List<DynamicColumn> dynamicColumns) throws IOException {
 		Map<DynamicColumn, Object> dynamicColumnsValues = new HashMap<>();
 		for (DynamicColumn dynamicColumn : dynamicColumns) {
-			System.out.println("Input value for the dynamic column " + dynamicColumn + " for : " + name);
+			System.out.println("---> Input value for the dynamic column '" + dynamicColumn + "' for : " + name);
 			String rawValue = reader.readLine();
 
 			if (String.class == dynamicColumn.getDynamicColumnType()) {
@@ -101,9 +88,7 @@ public class CfRunner {
 		}
 		
 		CustomFieldsCarManufacturer manufacturer = new CustomFieldsCarManufacturer(name, dynamicColumnsValues);
-		orm.update(manufacturer,
-				new UpdateSettings<>(dynamicColumns, null));
-
+		carService.createManufacturer(manufacturer, dynamicColumns);
 		return manufacturer;
 	}
 		
---
+++ src/main/java/com/asentinel/orm/demo/cf/service/CarService.java
@@ -0,0 +1,56 @@
+package com.asentinel.orm.demo.cf.service;
+
+import com.asentinel.common.jdbc.DefaultObjectFactory;
+import com.asentinel.common.orm.AutoEagerLoader;
+import com.asentinel.common.orm.OrmOperations;
+import com.asentinel.common.orm.mappers.dynamic.DynamicColumn;
+import com.asentinel.common.orm.mappers.dynamic.DynamicColumnsEntityNodeCallback;
+import com.asentinel.common.orm.persist.UpdateSettings;
+import com.asentinel.orm.demo.cf.domain.CarManufacturer;
+import com.asentinel.orm.demo.cf.domain.CarModel;
+import com.asentinel.orm.demo.cf.domain.CustomFieldsCarManufacturer;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CarService {
+
+    private final OrmOperations orm;
+
+    public CarService(OrmOperations orm) {
+        this.orm = orm;
+    }
+
+    @Transactional
+    public void addManufacturerField(String name, String type) {
+        orm.getSqlQuery()
+                .update("alter table CarManufacturers add column " +  name + " " + type);
+    }
+
+    @Transactional
+    public void createManufacturer(CustomFieldsCarManufacturer manufacturer, List<DynamicColumn> attributes) {
+        orm.update(manufacturer,
+                new UpdateSettings<>(attributes, null));
+    }
+
+    @Transactional(readOnly = true)
+    public CarManufacturer findManufacturerByName(String name, List<DynamicColumn> attributes) {
+        return orm.newSqlBuilder(CustomFieldsCarManufacturer.class)
+                .select(
+                        AutoEagerLoader.forPath(com.asentinel.orm.demo.cf.domain.CarManufacturer.class, CarModel.class),
+                        new DynamicColumnsEntityNodeCallback<>(
+                                new DefaultObjectFactory<>(CustomFieldsCarManufacturer.class),
+                                attributes
+                        )
+                )
+                .where().column("name").eq(name)
+            .execForEntity();
+    }
+
+    @Transactional
+    public void createModels(CarModel... models) {
+        orm.update(models);
+    }
+}
